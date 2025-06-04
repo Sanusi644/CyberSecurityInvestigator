2a302703756a87902ab02857bb4b9081a5f026ae
Sanusi644 <109221870+Sanusi644@users.noreply.github.com>
2025-06-04 13:17:42 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-echo "Installing dependencies..."
-pkg update -y && pkg upgrade -y
-pkg install git python3 adb -y
-pip install colorama
-
-echo "Installation complete. You can now run the tool with: python3 cyber_investigator.py"
